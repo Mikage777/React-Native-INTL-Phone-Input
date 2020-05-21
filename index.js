b68b3e2b88498bf7eb1481bb7f46013b06df3d4c
Игорь Бахтин <mikage345@gmail.com>
2020-05-21 14:26:53 +0500

    clean

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 /**
  * @format
  */
-
-import { AppRegistry } from "react-native";
-import App from "./App";
-import { name as appName } from "./app.json";
 import IntlPhoneInput from "./src/IntlPhoneInput";
-
-AppRegistry.registerComponent(appName, () => App);
-
 export default IntlPhoneInput;
